002ff602a925949c7dad483e9b880ab83106cbd7
phalla <ahlaluktnam@gmail.com>
2026-06-06 23:26:30 +0700

    update route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,16 +4,19 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/auth/google', [AuthController::class, 'google']);
-
 Route::get('/auth/google/callback', [AuthController::class, 'googleCallback']);
 
 Route::post('/auth/register', [AuthController::class, 'register']);
-Route::middleware('auth:sanctum')->post('/auth/logout', [AuthController::class, 'logout']);
+Route::post('/auth/login', [AuthController::class, 'login']);
 Route::post('/auth/verify-otp', [AuthController::class, 'verifyOtp']);
 
-// ADMIN ROUTES
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
+
+});
+
 Route::middleware(['auth:sanctum', 'admin'])->group(function () {
-    Route::post('/auth/login', [AuthController::class, 'login']);
 
     Route::get('/admin/dashboard', function () {
         return response()->json([
@@ -21,4 +24,4 @@ Route::middleware(['auth:sanctum', 'admin'])->group(function () {
         ]);
     });
 
-}); 
+});
